b3415d4e0830b1c3d6400e93310285fd496e9411
Ben Rodenhäuser <ben@karo.berlin>
2018-02-02 08:03:35 +0100

    view templates

---
+++ app.rb
@@ -0,0 +1,40 @@
+require_relative 'advice'
+
+class App
+  def erb(filename, local = {})
+    b = binding
+    message = local[:message]
+    content = File.read("views/#{filename}.erb")
+    ERB.new(content).result(b)
+  end
+
+  def response(status, headers, body = '')
+    body = yield if block_given?
+    [status, headers, [body]]
+  end
+
+  # the env hash is how the rack passes information about the request to our app, i.e., rack receives the request from Webrick and passes on the information about the request to our app, using the env hash. 
+  def call(env)
+    case env['REQUEST_PATH']
+    when '/'
+      status = '200'
+      headers = {"Content-Type" => 'text/html'}
+      response(status, headers) do
+        erb(:index)
+      end
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      status = '200'
+      headers = {"Content-Type" => 'text/html'}
+      response(status, headers) do
+        erb(:advice, message: piece_of_advice)
+      end
+    else
+      status = '404'
+      headers = {"Content-Type" => 'text/html', "Content-Length" => '61'}
+      response(status, headers) do
+        erb(:not_found)
+      end
+    end
+  end
+end
